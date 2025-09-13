a4a4b888200b4b06e847df9c7d0f0a4c27adfdd6
Ramy430 <edriskadil@yahoo.com>
2025-09-13 12:54:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,19 @@ searchBtn.addEventListener('click', () => {
 
 // Initial render
 renderDictionary(dictionary);
+const exportBtn = document.getElementById('exportBtn');
+
+exportBtn.addEventListener('click', () => {
+  const dataStr = JSON.stringify(dictionary, null, 2); // pretty format
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = "tausug_dictionary.json";
+  a.click();
+
+  URL.revokeObjectURL(url);
+});
+
+
